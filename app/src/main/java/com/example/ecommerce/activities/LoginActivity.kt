cb622bd148b9783a5b82992097b8921eed6d5d62
shimhm <55525917+shimhm@users.noreply.github.com>
2021-04-23 18:14:51 +0900

    Register page

--- app/src/main/java/com/example/ecommerce/activities/LoginActivity.kt
+++ app/src/main/java/com/example/ecommerce/activities/LoginActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.ecommerce.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +29,61 @@ class LoginActivity : AppCompatActivity() {
             )
         }
 
-        login_tv_register.setOnClickListener {
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(intent)
+    }
+
+    // Clickable components are login button, forgot password text and register text
+    override fun onClick(v: View?){
+        if(v != null) {
+            when (v.id) {
+                R.id.login_forgot_password -> {
+
+                }
+
+                R.id.login_btn_login -> {
+                    loginUser()
+                }
+
+                R.id.login_tv_register -> {
+                    val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(login_edt_email.text.toString().trim(){it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_email), true)
+                false
+            }
+            TextUtils.isEmpty(login_edt_password.text.toString().trim(){it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid", false)
+                true
+            }
+        }
+    }
+
+    private fun loginUser() {
+        if(validateLoginDetails()){
+
+            showProgress(resources.getString(R.string.please_wait))
+
+            val email = login_edt_email.text.toString().trim { it <= ' ' }
+            val password = login_edt_password.text.toString().trim { it <= ' ' }
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+
+                        showErrorSnackBar("You are logged in successfully.", false)
+                    }
+                }
+
         }
     }
 }
